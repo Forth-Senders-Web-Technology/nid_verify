86e3b3bdd28000041d2aa6875985cf6dfa470304
Masud <masudapon4@gmail.com>
2021-03-08 00:44:49 +0600

    first update

--- application/views/body.php
+++
@@ -1,4 +0,0 @@
-<h1>Body File</h1>
-
-
-<?php echo $kalam; ?>
---
+++ application/views/front/people.php
@@ -0,0 +1,56 @@
+<div class="card pd-20 pd-xs-30 shadow-base bd-0 mg-t-30">
+                <h6 class="tx-gray-800 tx-uppercase tx-semibold tx-13 mg-b-30">People You May Know</h6>
+                <div class="media-list">
+                  <div class="media align-items-center pd-b-10">
+                    <img src="http://via.placeholder.com/280x280" class="wd-45 rounded-circle" alt="">
+                    <div class="media-body mg-x-15 mg-xs-x-20">
+                      <h6 class="mg-b-2 tx-inverse tx-14">Marilyn Tarter</h6>
+                      <p class="mg-b-0 tx-12">@marilyntarter</p>
+                    </div>
+                    <a href="#" class="btn btn-outline-secondary btn-icon rounded-circle mg-r-5">
+                      <div><i class="icon ion-android-person-add tx-16"></i></div>
+                    </a>
+                  </div>
+                  <div class="media align-items-center pd-y-10">
+                    <img src="http://via.placeholder.com/280x280" class="wd-45 rounded-circle" alt="">
+                    <div class="media-body mg-x-15 mg-xs-x-20">
+                      <h6 class="mg-b-2 tx-inverse tx-14">Belinda Connor</h6>
+                      <p class="mg-b-0 tx-12">@bconnor</p>
+                    </div>
+                    <a href="#" class="btn btn-outline-secondary btn-icon rounded-circle mg-r-5">
+                      <div><i class="icon ion-android-person-add tx-16"></i></div>
+                    </a>
+                  </div>
+                  <div class="media align-items-center pd-y-10">
+                    <img src="http://via.placeholder.com/280x280" class="wd-45 rounded-circle" alt="">
+                    <div class="media-body mg-x-15 mg-xs-x-20">
+                      <h6 class="mg-b-2 tx-inverse tx-14">Deborah Miner</h6>
+                      <p class="mg-b-0 tx-12">@dminer</p>
+                    </div>
+                    <a href="#" class="btn btn-outline-secondary btn-icon rounded-circle mg-r-5">
+                      <div><i class="icon ion-android-person-add tx-16"></i></div>
+                    </a>
+                  </div>
+                  <div class="media align-items-center pd-y-10">
+                    <img src="http://via.placeholder.com/280x280" class="wd-45 rounded-circle" alt="">
+                    <div class="media-body mg-x-15 mg-xs-x-20">
+                      <h6 class="mg-b-2 tx-inverse tx-14">Theodore Grestin</h6>
+                      <p class="mg-b-0 tx-12">@theodore</p>
+                    </div>
+                    <a href="#" class="btn btn-outline-secondary btn-icon rounded-circle mg-r-5">
+                      <div><i class="icon ion-android-person-add tx-16"></i></div>
+                    </a>
+                  </div>
+                  <div class="media align-items-center pd-y-10">
+                    <img src="http://via.placeholder.com/280x280" class="wd-45 rounded-circle" alt="">
+                    <div class="media-body mg-x-15 mg-xs-x-20">
+                      <h6 class="mg-b-2 tx-inverse tx-14">Andrew Wiggins</h6>
+                      <p class="mg-b-0 tx-12">@awiggins</p>
+                    </div>
+  
+                    <a href="#" class="btn btn-outline-secondary btn-icon rounded-circle mg-r-5">
+                      <div><i class="icon ion-android-person-add tx-16"></i></div>
+                    </a>
+                  </div><!-- media -->
+                </div><!-- media-list -->
+              </div>
