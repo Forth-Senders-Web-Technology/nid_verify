1d3f58a580904ab8741e8b708439164d40629f71
khobayer <khobayerkhan@gmail.com>
2021-03-08 00:13:38 +0600

    nid verify page design

--- application/views/admin/nid_verify_view.php
+++ application/views/admin/nid_verify_view.php
@@ -1,4 +1,3 @@
-
     <!-- ########## START: MAIN PANEL ########## -->
     <div class="br-mainpanel">
       <div class="br-pageheader pd-y-15 pd-l-20">
@@ -7,22 +6,49 @@
           <span class="breadcrumb-item active">NID Verify</span>
         </nav>
       </div><!-- br-pageheader --> 
-      <div class="br-pagebody">
+      <!-- < class="br-pagebody"> -->
 
+        <div class="d-flex justify-content-center" style="margin-top: 40px; margin-bottom: 30px">
+          <div class="input-group wd-xs-300">
+            <input type="text" class="form-control" placeholder="Search">
+            <div class="input-group-btn">
+              <button class="btn btn-info"><i class="fa fa-search"></i></button>
+            </div><!-- input-group-btn -->
+          </div><!-- input-group -->
+        </div><!-- d-flex -->
 
+        
 
+        <div> 
+          <center>
+            <div style="width: 150px; margin-bottom: 30px;">
+              <img src="https://via.placeholder.com/150" class="card-img-top" alt="image" style="">
+            </div>
+          </center>
+          <center>
+            <div style="width: 50%;">
+                <table class="table">
+                   <!-- class="thead-info" -->
+                      <tr>
+                        <th style="border: 1px solid; background-color: #17A2B8; color: white; text-align: right;">Name</th>
+                        <td style="color: black; padding-left: 30px;">Md. Rahim Murol</td>
+                      </tr>
 
+                      <tr>
+                        <th style="border: 1px solid; background-color: #17A2B8; color: white; text-align: right;">Father's Name</th>
+                        <td style="color: black; padding-left: 30px;">Md. Karim Murol</td>
+                      </tr>
 
-          
-          <!-- start you HTML Code here -->
-
-
-
-
-
-
-
-
+                      <tr>
+                        <th style="border: 1px solid; background-color: #17A2B8; color: white; text-align: right;">Mother's Name</th>
+                        <td style="color: black; padding-left: 30px;">Jorina Khatun</td>
+                      </tr>
+      </div>
+      </center>
+        </div>
+        
+      
+      
 
       </div><!-- br-pagebody -->
     </div><!-- br-mainpanel -->
